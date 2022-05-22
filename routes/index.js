225f8ec5c6dc86a3851c6efd63b3cc1316494255
pandula12 <pandulagajadeera09@gmail.com>
2022-05-22 20:34:58 +0930

    prac 8 final

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/actors', function(req, res){
-  req.pool.getConnection(function(err,connection) {
+router.get('/actors', function (req, res) {
+  req.pool.getConnection(function (err, connection) {
     if (err) {
       res.sendStatus(500);
       return;
     }
     var query = "SELECT first_name, last_name FROM actor";
-    connection.query(query, function(err, rows, fields) {
+    connection.query(query, function (err, rows, fields) {
       connection.release();
       if (err) {
         res.sendStatus(500);
@@ -22,6 +22,28 @@ router.get('/actors', function(req, res){
       res.json(rows);
     });
   });
+});
+
+router.post('/addactor', function (req, res) {
+  if (req.body.hasOwnProperty("firstName") && req.body.hasOwnProperty("lastName")) {
+    var first = req.body.firstName;
+    var last = req.body.lastName;
+    req.pool.getConnection(function (err, connection) {
+      if (err) {
+        res.sendStatus(500);
+        return;
+      }
+      var query = "INSERT INTO actor (first_name, last_name) VALUES (?, ?);";
+      connection.query(query, [first, last], function (err, rows, fields) {
+        connection.release();
+        if (err) {
+          res.sendStatus(500);
+          return;
+        }
+      });
+    });
+  }
+  res.send();
 });
 
 module.exports = router;
